152e5b1e4f861f20bc59ba53937ee7321b2d4c54
vakosta <v.annkv@ya.ru>
2023-01-08 23:24:34 +0300

    Initial commit

---
+++ src/dotnet/ReSharperPlugin.CodeCommenter/SampleQuickFixesRegistrar.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Application;
+using JetBrains.Lifetimes;
+using JetBrains.ReSharper.Feature.Services.QuickFixes;
+using JetBrains.Util;
+
+namespace ReSharperPlugin.CodeCommenter;
+
+[ShellComponent]
+internal class SampleQuickFixesRegistrar : IQuickFixesProvider
+{
+    public void Register(IQuickFixesRegistrar registrar)
+    {
+        registrar.RegisterQuickFix<SampleHighlighting>(
+            Lifetime.Eternal,
+            h => new SampleQuickFix(h.Declaration),
+            typeof(SampleQuickFix));
+    }
+
+    public IEnumerable<Type> Dependencies => EmptyArray<Type>.Instance;
+}
